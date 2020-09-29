82288848ba4286a82db1a8d53e30a91c90e81921
Nadya Zhernak <makaronchik28@gmail.com>
2020-09-29 18:18:33 +0300

    feat: add min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined) {
+        return 0;
+    }
+    let min = 0;
+    for (let i = 0; i < array.length; i++) {
+        min = ( array[i] < min ) ? array[i] : min;
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array) { 
+    if (array === undefined) {
+        return 0;
+    }
+    let max = 0;
+    for (let i = 0; i < array.length; i++) {
+        max = ( array[i] > max ) ? array[i] : max;
+    }
+    return max;  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    } else {
+        let average =  0; 
+        for (let i = 0; i < array.length; i++) {
+            average += array[i]; 
+        }
+        return average / array.length;
+    }
 }
